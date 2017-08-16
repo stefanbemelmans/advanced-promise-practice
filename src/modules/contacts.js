0625c9cb678a52cc4f6a4011e2b40b76ccbaa19a
Jon Woo <jw56578@gmail.com>
2017-08-16 16:27:26 -0500

    remove comments

--- src/modules/contacts.js
+++ src/modules/contacts.js
@@ -13,20 +13,22 @@ function jsonIsReady(data) {
 }
 
 function loadAllContacts() {
-  // 1) I want to fetch all contacts from a server
-  // where is this information located?
   const webRequestPromise = fetch("/contacts");
-  // 2) my code is going to continue to run while the web request is still going
-  // 3) I need to tell the promise what function to run when the web request is done
   const getJsonPromise = webRequestPromise.then(theWebRequstIsDone);
-  // 4) I need to tell the promise what function to run when the json is ready
   getJsonPromise.then(jsonIsReady);
-
-
 }
 loadAllContacts();
 
-// 3) We can use promises from other modules
+
+
+fetch("/contacts").then(function (response) {
+  return response.json();
+}).then(function (data) {
+  // do something with data
+  console.log(data);
+});
+
+
 getOneContact(1).then(function (data) {
   document.getElementById("contactName").innerHTML = data.name;
 });
