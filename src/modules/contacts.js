a4a1105f59a55da5fe99428c9644f0f64e67f494
stackbaxter <stackbaxter@yahoo.com>
2017-08-20 22:59:38 -0500

    add comments is left, it's all coming back to me now.

--- src/modules/contacts.js
+++ src/modules/contacts.js
@@ -24,7 +24,8 @@ loadAllContacts();
 fetch("/contacts").then(function (response) {
   return response.json();
 }).then(function (data) {
-  // do something with data
+  document.getElementById("numberOfContacts2").innerHTML = data.length;
+  
   console.log(data);
 });
 
